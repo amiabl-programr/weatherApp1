9f09ee4a9c2a4c265cd644e9b37c552033440aa6
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-03-09 09:05:16 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
 import cloud from "./assets/cloud.png";
+import search_icon from "./assets/search-location.svg";
 
 
 
 const App = () => {
   const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("Lagos");
   const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
   const URL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
   // use string literals for the api key
 
-  const handleSearch = (e) => {
-    e.preventDefault();
+  const handleSearch = (event) => {
+    event.preventDefault();
     fetch(URL)
       .then((response) => response.json())
       .then((data) => {
@@ -22,10 +24,20 @@ const App = () => {
         // console.log(data?.main?.pressure)
       })
       .catch((err) => console.error(err))
-    setLocation("")
+    // setLocation("")
 
   }
-
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data)
+        console.log(data)
+        // console.log(data?.main?.pressure)
+      })
+      .catch((err) => console.error(err))
+    setLocation("");
+  }, [])
   // function to handle weather search
   // const handleCurrentLocation = (e) => {
   //   e.preventDefault();
@@ -35,68 +47,86 @@ const App = () => {
 
   return (
     <>
+      <header>
+        <nav>
+          <h1>Weather</h1>
+
+
+          <div className="nav__links">
+            <a href="#">Git</a>
+            <a href="#">Twitter</a>
+          </div>
+
+        </nav>
+      </header>
       <main>
-        {/* header */}
-        <header>
-          <nav>
-            <button>Forecast</button>
-            <ul>
-              <li>Git</li>
-              <li>Twitter</li>
-              <form onSubmit={handleSearch}>
-                <label>
-                  <input
-                    type="text"
-                    // value=""
-                    onClick={handleSearch}
-                    onChange={(e) => setLocation(e.target.value)}
-                    // onKeyPress={handleSearch}
-                    name="location"
-                    placeholder="Enter location"
-                  />
-                </label>
-              </form>
-            </ul>
-          </nav>
-        </header>
+        <form onSubmit={handleSearch}>
+          <label>
+            <input
+              type="text"
+              // value=""
+              // onke={handleSearch}
+              onChange={(e) =>
+                setLocation(e.target.value)
+              }
+              // onKeyPress={handleSearch}
+              name="location"
+              placeholder="Enter location"
+            />
+            <img src={search_icon} alt="" onClick={handleSearch} className="search__icon" />
+          </label>
+        </form>
 
 
         <div className="weather__info">
-          <h1 className="city__name">{data.name}</h1>
+          <h1 className="city__name">Lagos{data.name}</h1>
           <div className="temp__container">
             <span className="temp">
-              23<sup>o</sup>c
+              {data.main && data.main.temp}
+              <sup>o</sup>c
             </span>
 
-            <img src={cloud} className="temp__image" alt="temperature" />
+            {/* <img src={cloud} className="temp__image" alt="temperature" /> */}
           </div>
         </div>
 
-        <section>
-          {data.main && (
+        <div className="cards__container">
+          {data.main ? (
             <>
-
-              <div className="pressure">
-                {data.main.pressure}
-                pressure
-              </div>
-              <div className="humidity">
-                {data.main.humidity}
-                humidity
-              </div>
-              <div className="visibility">
-                {data.visibility}
-                visibility
-              </div>
-              <div className="windspeed">
-                {data.wind.speed}
-                windspeed
+              <div className="cards">
+                <div>
+                  <h2>Pressure</h2>
+                  {data.main.pressure}
+                </div>
+                <div>
+                  <h2>
+                    Humidity
+                  </h2>
+                  {data.main.humidity}
+                </div>
+                <div>
+                  <h2>
+                    Visibility
+                  </h2>
+                  {data.visibility}
+                </div>
+                <div>
+                  <h2>
+                    Windspeed
+                  </h2>
+                  {data.wind.speed}
+                </div>
               </div>
             </>
 
-          )}
-        </section>
+          )
+            :
+            <p>Loading...</p>}
+        </div>
       </main>
+      <footer>
+        By Victor
+      </footer>
     </>
   );
 }
